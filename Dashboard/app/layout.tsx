2516a2d6e2d7325823bfea48b7e64050c71df459
aneeshpatne <aneeshpatne@gmail.com>
2025-04-03 18:38:42 +0530

    refactor: remove SmtpMessage component and clean up auth pages layout

--- Dashboard/app/layout.tsx
+++ Dashboard/app/layout.tsx
@@ -2,6 +2,8 @@ import HeaderAuth from "@/components/header-auth";
 import { ThemeSwitcher } from "@/components/theme-switcher";
 import { Geist } from "next/font/google";
 import { ThemeProvider } from "next-themes";
+import { House } from "lucide-react";
+import Link from "next/link";
 import "./globals.css";
 
 const defaultUrl = process.env.VERCEL_URL
@@ -36,15 +38,18 @@ export default function RootLayout({
           <main className="min-h-screen flex flex-col items-center">
             <div className="flex-1 w-full flex flex-col gap-20 items-center">
               <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
-                <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-                  <div className="flex gap-5 items-center font-semibold"></div>
+                <div className="w-full flex justify-between items-center p-3 px-5 text-sm">
+                  <Link
+                    href="/"
+                    className="font-semibold flex items-center gap-2"
+                  >
+                    <House className="h-4 w-4" />
+                  </Link>
                   <HeaderAuth />
                   <ThemeSwitcher />
                 </div>
               </nav>
-              <div className="flex flex-col gap-20 max-w-5xl p-5">
-                {children}
-              </div>
+              <div className="">{children}</div>
             </div>
           </main>
         </ThemeProvider>
