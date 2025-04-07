7312ec67a17217b7d19dc9a05cbffdfa6a5212b7
aneeshpatne <aneeshpatne@gmail.com>
2025-04-07 11:56:09 +0530

    feat: implement URL redirection and enhance error handling for short URLs

--- Dashboard/app/api/url/[slug]/route.ts
+++ Dashboard/app/api/url/[slug]/route.ts
@@ -1,29 +1,43 @@
+import { NextRequest } from "next/server";
 import { PrismaClient } from "@prisma/client";
 import { createClient } from "@/utils/supabase/server";
+
 const prisma = new PrismaClient();
-interface Params {
-  slug: string;
-}
-export async function GET(req: Request, context: { params: Params }) {
+
+export async function GET(
+  req: NextRequest,
+  { params }: { params: { slug: string } }
+) {
   const supabase = await createClient();
   const {
     data: { user },
   } = await supabase.auth.getUser();
+
   if (!user) {
-    return Response.json({ error: "Unauthorized" }, { status: 401 });
+    return new Response(JSON.stringify({ error: "Unauthorized" }), {
+      status: 401,
+    });
   }
 
   try {
-    const { slug } = await context.params;
+    const { slug } = params;
     const links = await prisma.shortUrl.findUnique({
       where: { shortUrl: slug },
     });
+
     if (!links) {
-      return Response.json({ error: "Does Not Exist" }, { status: 404 });
+      return new Response(JSON.stringify({ error: "Does Not Exist" }), {
+        status: 404,
+      });
     }
-    return Response.json({ links }, { status: 200 });
+
+    return new Response(JSON.stringify({ links }), {
+      status: 200,
+    });
   } catch (err) {
-    console.log(err);
-    return Response.json({ error: "Something went wrong" }, { status: 500 });
+    console.error(err);
+    return new Response(JSON.stringify({ error: "Something went wrong" }), {
+      status: 500,
+    });
   }
 }
--- server/server.js
+++ server/server.js
@@ -1,3 +1,27 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
 import cors from "cors";
+const app = express();
+app.use(cors());
+const PORT = process.env.PORT || 3001;
+const prisma = new PrismaClient();
+
+app.get("/:slug", async (req, res) => {
+  const { slug } = req.params;
+  try {
+    const record = await prisma.shortUrl.findUnique({
+      where: { shortUrl: slug },
+    });
+    if (!record) {
+      return res.status(404).json({ error: "Record not found" });
+    }
+    return res.redirect(302, record.longUrl);
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
